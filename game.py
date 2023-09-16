6f8c6b71759335e83724c3e6b55f7f70cccee6bc
DonAllenII <don.a.alexanderii@gmail.com>
2023-09-16 16:29:25 -0400

    created choices dict

---
+++ game.py
@@ -0,0 +1,4 @@
+#create a dictionary of choices, key beats 1st value & loses to 2nd value in list
+choices = {"rock": ["scissors", "paper"],
+          "paper": ["rock", "scissors"],
+        "scissors": ["paper", "rock"]}
